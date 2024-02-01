624ba1f50551e6904c9c2018fe4447dab9778b01
s21sd <srivastavasunny359@gmail.com>
2024-02-01 17:09:58 +0530

    "Updated"

---
+++ Backend/middlewares/checkauthMiddle.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+function checkAuth(req, res, next) {
+    const authToken = req.cookies.authToken;
+    const refreshToken = req.cookies.refreshToken;
+    console.log("Check Auth Token MIDDLEWARE ")
+    if (!authToken || refreshToken) {
+        return res.status(401).json({
+            message: 'Authentication failed: No authToken or refreshToken provided', ok: false
+        })
+    }
+    jwt.verify(authToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET_KEY, (refreshErr, refreshDecoded) => {
+                if (refreshErr) {
+                    return res.status(401).json({
+                        message: 'Auth failed',
+                        ok: false
+                    })
+                }
+                else {
+                    const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '10m' });
+                    const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET_KEY, { expiresIn: '50m' });
+
+                    res.cookie('authToken', authToken, { httpOnly: true });
+                    res.cookie('refreshToken', refreshToken, { httpOnly: true });
+
+                    res.userId = refreshDecoded.userId;
+                    req.ok = true;
+                    req.message = 'Auth Successfully';
+                    next();
+
+                }
+            })
+        }
+        else {
+            req.userId = decoded.userId;
+            req.ok = true;
+            req.message = "Authentication successful";
+            next();
+
+        }
+    })
+}
+
+module.exports = checkAuth;
--- Backend/routes/authroutes.js
+++ Backend/routes/authroutes.js
@@ -7,9 +7,11 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const nodemailer = require('nodemailer');
 const resfunction = require('../utils/resfunction');
+const authTokenHandler = require('../middlewares/checkauthMiddle');
 const fs = require('fs');
 const errormiddlewares = require('../middlewares/errorMiddle');
 const checkauthmiddlewares = require('../middlewares/checkauthMiddle');
+const verificationModel = require('../Models/verificationModel');
 
 async function mailer(recievemail, code) {
     let transporter = nodemailer.createTransport({
@@ -87,8 +89,110 @@ const fileUploaFunction = (req, res, next) => {
     })
 }
 router.post('/register', fileUploaFunction, async (req, res, next) => {
-    console.log(req.file);
+    // console.log(req.file);
+    try {
+        const { name, email, password, otp } = req.body;
+        let user = await User.findOne({ email: email });
+        let varificationQueue = await Verification.findOne({ email: email });
+
+        if (user) {
+            if (req.file && req.file.path) {
+                fs.unlink(req.file.path, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log('File deleted successfully');
+                    }
+                })
+            }
+        }
+
+        if (!varificationQueue) {
+            if (req.file && req.file.path) {
+                fs.unlink(req.file.path, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log('File deleted successfully');
+                    }
+                })
+            }
+            return resfunction(res, 400, 'Please send otp first', null, false);
+        }
+        const isMatch = await bcrypt.compare(otp, varificationQueue.code);
+        if (!isMatch) {
+            if (req.file && req.file.path) {
+                fs.unlink(req.file.path, (err) => {
+                    if (err) {
+                        console.error('Error deleting file ', err);
+                    }
+                    else {
+                        console.log('File deleted successfully');
+                    }
+                })
+            }
+
+        }
+        user = new User({
+            name: name,
+            email: email,
+            password: password,
+            profilePic: req.file.path
+        })
+        await user.save();
+        await Verification.deleteOne({ email: email });
+        return resfunction(res, 200, 'User Registered successfully', null, false);
+
+
+    } catch (error) {
+        console.log(err);
+        return resfunction(res, 500, 'Internal server error', null, false);
+    }
 })
 
+
+router.post('/login', async (req, res, next) => {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+        return resfunction(res, 404, 'Invalid Credentials', null, false);
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        return resfunction(res, 404, 'Invalid Credentials', null, false);
+
+    }
+    const authToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, { expiresIn: '10m' });
+    const refreshToken = jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET_KEY, { expiresIn: '50m' });
+
+    res.cookie('authToken', authToken, { httpOnly: true });
+    res.cookie('refreshToken', refreshToken, { httpOnly: true });
+    return resfunction(res, 200, 'User logged in successfully', {
+        authToken,
+        refreshToken
+    }, true);
+
+})
+
+
+router.get('/checklogin', authTokenHandler, async (req, res, next) => {
+    res.json({
+        ok: req.ok,
+        message: req.message,
+        userId: req.userId
+    })
+})
+
+router.post('/logout', authTokenHandler, async (req, res, next) => {
+    req.clearCookie('authToken');
+    req.clearCookie('refreshToken');
+    res.json({
+        ok: true,
+        message: 'Logged out successfully'
+    })
+
+})
 
 module.exports = router;
